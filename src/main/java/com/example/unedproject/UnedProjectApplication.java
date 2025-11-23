c012484915238ba82d75e4b6a1bf803db04613bc
naragoki <naragomezkim@hotmail.com>
2025-11-23 16:55:54 -0600

    Added initial configuration

---
+++ src/main/java/com/example/unedproject/UnedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.unedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnedProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unedproject/UnedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
